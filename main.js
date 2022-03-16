bb2e2f9ec134113789bbea49722c4ea97359158c
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-03-16 15:09:46 +0300

    js tuts

---
+++ main.js
@@ -0,0 +1,42 @@
+
+ console.log('hey this works');
+ //alert('hey this works')
+
+//document.getElementById('someText').innerHTML = age;
+
+
+ //prompt('hello, whats your name')
+
+ var b = 'smoothie';
+ console.log(b);
+
+ var someNumber = '45';
+ console.log(someNumber);
+
+ //var age = prompt('hello, whats your age');
+ //document.getElementById('someText').innerHTML = age;
+
+ //numbers
+//increase the number
+ var num1 = 9.57;
+ num1++;
+ console.log(num1);
+
+
+ //console.log(50*8)
+
+ //decrease the number
+ //someNumber--;
+ //console.log(someNumber);
+
+ //devide multiply*, remainders %
+ //console.log(someNumber % 7)
+
+ //invrement by any no.
+ num1 += 7;
+ console.log(num1);
+
+ someNumber -= 7;
+ console.log(someNumber);
+ 
+ 
