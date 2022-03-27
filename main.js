8a0c0e1cb75b094e9eac2ec8f09d037369fb3068
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-03-27 17:14:24 +0300

    wip

--- main.js
+++ main.js
@@ -156,3 +156,27 @@ if ((age>= 18) && (age<= 35)) {
 // day 0 --> Sunday
 // day 6 --> Saturday -->
 // day 4 --> Thursday --> weekday
+
+
+const indicator = document.querySelector("[data-indicator]")
+
+document.addEventListener("click", e => {
+  let anchor
+  if (e.target.matches("a")) {
+    anchor = e.target
+  } else {
+    anchor = e.target.closest("a")
+  }
+  if (anchor != null) {
+    const allAnchors = [...document.querySelectorAll("a")]
+    const index = allAnchors.indexOf(anchor)
+    indicator.style.setProperty("--position", index)
+    document.querySelectorAll("a").forEach(elem => {
+      elem.classList.remove("active")
+    })
+    anchor.classList.add("active")
+  }
+})
+
+
+
